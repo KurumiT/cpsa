1c778570b2ddaa53be4a4461a77afcae3c9d91ab
h3lpkey <willekb@gmail.com>
2020-05-22 23:20:11 +0500

    init

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { View, Text } from "react-native";
+import Hello from "./src/components/Hello";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         alignItems: "center",
       }}
     >
+      <Hello />
       <Text>Universal React with Expo</Text>
     </View>
   );
